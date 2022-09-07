358de96ea025e47511df0653e85498f68909fe9c
guilt <gulilty4all@gmail.com>
2022-09-07 16:34:16 +0600

    Solution

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,12 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        String symbol = "8";
+        int space = cathetusLength;
+        for (int i = 1; i <= cathetusLength; i++) {
+            space -= 1;
+            System.out.println(" ".repeat(space) + symbol.repeat(i));
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        String line = "";
+        int sp ;
+        for(int i = 1; i <= cathetusLength; i++){
+            sp = cathetusLength * 2;
+            if (i == 1 ){
+                line = Integer.toString(i);
+                sp = (sp - line.length())/2;
+                System.out.println(" ".repeat(sp)+line+" ".repeat(sp));
+            }else{
+                line = Integer.toString(i ) + line + Integer.toString(i );
+                sp = (sp - line.length())/2;
+                System.out.println(" ".repeat(sp)+ line + " ".repeat(sp));
+            }
+        }
     }
 
     public static void main(String[] args) {
