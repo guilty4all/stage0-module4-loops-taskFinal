98c4a0e016273b2065b13806f91c1752b551179c
guilt <gulilty4all@gmail.com>
2022-09-07 16:37:35 +0600

    Solution some fixes

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-        String symbol = "8";
+        String symbol = "*";
         int space = cathetusLength;
         for (int i = 1; i <= cathetusLength; i++) {
             space -= 1;
