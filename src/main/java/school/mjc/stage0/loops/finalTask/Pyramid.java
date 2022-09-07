a7029207216a601d8808e25c68f8e409ce5bad97
guilt <gulilty4all@gmail.com>
2022-09-07 16:53:57 +0600

    Solution some fixes2

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,18 +3,21 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
         String line = "";
-        int sp ;
-        for(int i = 1; i <= cathetusLength; i++){
+        int sp;
+        String result = "";
+        for (int i = 1; i <= cathetusLength; i++) {
             sp = cathetusLength * 2;
-            if (i == 1 ){
+            if (i == 1) {
                 line = Integer.toString(i);
-                sp = (sp - line.length())/2;
-                System.out.println(" ".repeat(sp)+line+" ".repeat(sp));
-            }else{
-                line = Integer.toString(i ) + line + Integer.toString(i );
-                sp = (sp - line.length())/2;
-                System.out.println(" ".repeat(sp)+ line + " ".repeat(sp));
+                sp = (sp - line.length()) / 2;
+                result =  " ".repeat(sp) + line + " ".repeat(sp) + "\n";
+            } else {
+                line = Integer.toString(i) + line + Integer.toString(i);
+                sp = (sp - line.length()) / 2;
+                result =  " ".repeat(sp) + line + " ".repeat(sp) + "\n";
             }
+            System.out.print(result);
+        }
         }
     }
 
